bbd4c7e7c9383a1ce39553a3b455a4e1e2a837a7
KAZUE ANAN <zakscloset@gmail.com>
2018-07-11 11:59:52 -0400

    server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+  app = express(),
+  path = require('path'),
+  port = process.env.PORT || 3000;
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')))
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`)
+})
