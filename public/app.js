dd86a08121d8d8b385531e06cfa4db9c123453c1
KAZUE ANAN <zakscloset@gmail.com>
2018-07-11 13:44:42 -0400

    events

--- public/app.js
+++ public/app.js
@@ -4,11 +4,32 @@ new Vue({
     name: 'kaz',
     job: 'jedi',
     website: 'http://www.google.com',
-    websiteTag: '<a href="http://www.google.com">google</a>'
+    websiteTag: '<a href="http://www.google.com">google</a>',
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(time){
       return `Good ${time}, ${this.name}!`
+    },
+    add: function(){
+      this.age++;
+    },
+    subtract: function(){
+      this.age--;
+    },
+    addBy: function(increment){
+      this.age += increment;
+    },
+    subtractBy: function(increment){
+      this.age -= increment;
+    },
+    updateXY: function(event){
+      // get the click event object
+      console.log(event)
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 })
