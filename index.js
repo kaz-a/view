6cf776a0ee3e828d8acd75f708340d9375ea3a40
KAZUE ANAN <zakscloset@gmail.com>
2018-07-12 11:05:28 -0400

    component

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use('/vendor', express.static(path.join(__dirname, 'node_modules')))
 app.use('/img', express.static(path.join(__dirname, 'assets/img')))
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  // res.sendFile(path.join(__dirname, 'basic.html'))
+  res.sendFile(path.join(__dirname, 'punchgame.html'))
+  // res.sendFile(path.join(__dirname, 'component.html'))
 })
 
 app.listen(port, () => {
