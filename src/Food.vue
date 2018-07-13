d7aaac45a5be40c6e33d824168a04b8fc4c5d826
KAZUE ANAN <zakscloset@gmail.com>
2018-07-13 13:44:05 -0400

    style scoped

--- src/Food.vue
+++ src/Food.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul>
-    <li v-for="food in foods">{{ food }}</li>
-  </ul>
+  <div>
+    <h1>List of foods</h1>
+    <ul>
+      <li v-for="food in foods">{{ food }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -14,6 +17,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1{
+    color: green;
+  }
 </style>
